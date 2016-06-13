8d609b6bcb937e663007f309742ac8388898ccde
Luís Fernando <luisfernando.pereira.moraes@gmail.com>
2016-06-12 23:41:48 -0300

    quase terminado

--- RodaARoda/rodaroda.c
+++ RodaARoda/rodaroda.c
@@ -47,31 +47,43 @@ int main()
 		while (palavra[index_qtdletras] != '\0') ++index_qtdletras; //calculando a quantidade de letras da palavra.
 		int qtd_letras = index_qtdletras; //strlen(palavra); 
 		int qtd_chutes = qtd_letras * 2;
-		int *letras_encontradas_p = (int*)malloc(sizeof(int) * qtd_letras);
+		char *letras_encontradas_p = (char*)malloc(sizeof(char) * qtd_letras);
 		int qtd_letras_econtradas = 0;
 
 		printf("O tema e: --- %s ----\n", tema);
+		for (int i = 0; i < qtd_letras; i++) printf("*");
+		printf("\n");
 		printf("=>\n");
-			
+
 
 		int acertou = 0;
 		do
 		{
 			printf("Informe uma letra: ");
 			char letra;
-			ler:
+		ler:
 			letra = getchar(); //Lendo a letra inputada.
-			if (letra == ' ')
+			if (letra == ' ' || letra == '\n')
 				goto ler;
 			int encontrou_letra = 0;
-				
+
+
+			for (int i = 0; i < qtd_letras; i++)
+			{
+				if (letra == letras_encontradas_p[i])
+				{
+					printf("Essa letra ja foi informada. Informe outra letra: ");
+					goto ler;
+				}
+			}
+
 
 			// Verificando se a letra existe na palavra
 			for (int i = 0; i < qtd_letras; i++)
 			{
 				if (palavra[i] == letra)
 				{
-					printf("deu boa, letra na posicao %i\n", i);
+					printf("letra %c na posicao %i\n", letra, i);
 					encontrou_letra = 1;
 					qtd_letras_econtradas++;
 					letras_encontradas_p[i] = letra;
@@ -87,15 +99,28 @@ int main()
 					else
 						printf("*");
 				}
-				printf("\n");
+				printf("\nAinda restam: %i chances\n", qtd_chutes);
 			}
 			else
 			{
-				printf("roda a roda...\n\n\n");
+				printf("\nAinda restam: %i chances\n", qtd_chutes);
+				//printf("roda a roda...\n\n\n");
 			}
 
 			qtd_chutes--; //usando operador de pos decremento
-		} while (acertou == 0 || qtd_chutes <= 0);
+		} while (acertou == 0 && qtd_chutes >= 0);
+		printf("Game Over. Jogar novamente? S/N: ");
+		char resposta;
+	ler2:
+		resposta = getchar();
+		if (resposta == 'n' || resposta == 'N')
+			exit(0);
+		else if (resposta == '\n')
+			goto ler2;
+		else if (resposta != 'S' || resposta != 's')
+		{
+			printf("Opcao incorreta.  Jogar novamente? S/N: ");
+			goto ler2;
+		}
 	} while (TRUE);
-
 }
